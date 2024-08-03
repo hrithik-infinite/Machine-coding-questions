6351226593c5fe46c5237ade98aa9ec297874ecc
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-08-03 11:36:35 +0530

    tic tac toe

---
+++ React/tic-tac-toe/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import TicTacToe from "./components/tic-tac-toe";
+
+function App() {
+  return <TicTacToe />;
+}
+
+export default App;
---
+++ React/tic-tac-toe/src/components/tic-tac-toe.jsx
@@ -0,0 +1,26 @@
+import useTicTacToe from "../hooks/use-tic-tac-toe";
+
+function TicTacToe() {
+  const { board, handleClick, calculateWinner, getStatusMessage, resetGame } = useTicTacToe();
+  return (
+    <div className="game">
+      <div className="status">
+        {getStatusMessage()}
+        <button className="reset-button" onClick={resetGame}>
+          Reset Game
+        </button>
+      </div>
+      <div className="board">
+        {board.map((b, index) => {
+          return (
+            <button className="cell" key={index} onClick={() => handleClick(index)} disabled={b !== null}>
+              {b}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default TicTacToe;
