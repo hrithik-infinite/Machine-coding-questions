87e561b069b65e252c07754a8d800dfef06130dc
Hrithik Agarwal <hrithikinfinite@gmail.com>
2025-03-13 07:24:31 +0530

    Modified progress bar

--- system-design-questions/progress-bar/src/App.jsx
+++ system-design-questions/progress-bar/src/App.jsx
@@ -1,22 +1,28 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import ProgressBar from "./Components/ProgressBar";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
   const [value, setValue] = useState(0);
-
   useEffect(() => {
     const interval = setInterval(() => {
       setValue((prev) => (prev >= 100 ? 0 : prev + 1));
     }, 50);
-
     return () => clearInterval(interval);
   }, []);
-
+  const ProgressBar = ({ value }) => {
+    return (
+      <div className="progress">
+        <div style={{ width: `${value > 8 ? value : 8}%` }}>
+          <span>{`${value} %`}</span>
+        </div>
+      </div>
+    );
+  };
   return (
-    <div className="app">
-      <ProgressBar width={value} />
-    </div>
+    <>
+      <h2>Progress Bar</h2>
+      <ProgressBar value={value} />
+    </>
   );
 }
 
