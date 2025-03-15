e38ab104dae7464fc557f380429b852f5d191103
Hrithik Agarwal <hrithikinfinite@gmail.com>
2025-03-15 22:48:05 +0530

    updated dropdown

--- system-design-questions/dropdown/src/App.jsx
+++ system-design-questions/dropdown/src/App.jsx
@@ -1,18 +1,50 @@
-import "./App.css";
-import DropDown from "./Components/DropDown";
+import { useState } from "react";
+
+const DropDown = ({ data = [], onSelect }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [val, setVal] = useState("");
+
+  const onSelectVal = (selectedValue) => {
+    setVal(selectedValue);
+    setIsOpen(false);
+    onSelect(selectedValue);
+  };
+
+  const toggleDropDown = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="dropDown">
+      <div className="dropdown-container" onClick={toggleDropDown}>
+        {val || "Select an option"}
+        <span className="dropdown-arrow">{isOpen ? "▲" : "▼"}</span>
+      </div>
+      {isOpen && (
+        <ul className="dropdown-list">
+          {data.map((option, i) => (
+            <li className="dropdown-item" key={i} onClick={() => onSelectVal(option)}>
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 function App() {
-  const data = {
-    title: "Title 01",
-    options: ["Option 01", "Option 02", "Option 03", "Option 04", "Option 05", "Option 06"],
+  const data = ["Option 01", "Option 02", "Option 03", "Option 04", "Option 05", "Option 06"];
+
+  const handleSelection = (selectedData) => {
+    console.log(selectedData);
   };
-  const handleSelection = data => {
-    alert(data);
-  };
+
   return (
-    <div>
-      <DropDown options={data.options} onSelect={handleSelection} />
-    </div>
+    <>
+      <h2>Dropdown</h2>
+      <DropDown data={data} onSelect={handleSelection} />
+    </>
   );
 }
 
