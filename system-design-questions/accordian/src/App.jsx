05e53064450c93e4974aa39aa37ef20c1467a39b
Hrithik Agarwal <hrithikinfinite@gmail.com>
2025-03-16 17:34:13 +0530

    updated accordian

--- system-design-questions/accordian/src/App.jsx
+++ system-design-questions/accordian/src/App.jsx
@@ -1,28 +1,52 @@
-import "./App.css";
-import Accordian from "./components/Accordian";
+import { useState } from "react";
 
+const data = [
+  {
+    header: "Header 01",
+    content: "Content 01"
+  },
+  {
+    header: "Header 02",
+    content: "Content 02"
+  },
+  {
+    header: "Header 03",
+    content: "Content 03"
+  },
+  {
+    header: "Header 04",
+    content: "Content 04"
+  }
+];
+const Accordian = ({ data }) => {
+  const [clickedIndex, setClickedIndex] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const handleHeaderClick = (index) => {
+    if (!isOpen) {
+      setClickedIndex(index);
+      setIsOpen(true);
+    } else {
+      setClickedIndex(null);
+      setIsOpen(false);
+    }
+  };
+  return (
+    <div className="mainContainer">
+      {data.map((val, i) => (
+        <div className="accordianContainer">
+          <div className="accordionHeader" key={i} onClick={() => handleHeaderClick(i)}>
+            {val.header}
+          </div>
+          {clickedIndex === i && <div className="accordionBody">{val.content}</div>}
+        </div>
+      ))}
+    </div>
+  );
+};
 function App() {
-  const data = [
-    {
-      header: "Header 01",
-      content: "Content 01",
-    },
-    {
-      header: "Header 02",
-      content: "Content 02",
-    },
-    {
-      header: "Header 03",
-      content: "Content 03",
-    },
-    {
-      header: "Header 04",
-      content: "Content 04",
-    },
-  ];
-
   return (
     <>
+      <h2>Accordian</h2>
       <Accordian data={data} />
     </>
   );
